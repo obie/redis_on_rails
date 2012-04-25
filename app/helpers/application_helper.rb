03396afbcd77bccc7ff751809da2c8a558bcf4a0
Obie Fernandez <dev+obie@hashrocket.com>
2012-04-24 20:15:10 -0500

    enhanced descriptions for events

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def enhanced_description(event)
+    event.description.tap do |d|
+      d.gsub!(event.conference.name, link_to(event.conference.name, url_for(event.conference)))
+      d.gsub!(event.attendee.name, link_to(event.attendee.name, url_for(event.attendee)))
+    end.html_safe
+  rescue
+    event.description
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,27 @@
 class Event < ActiveRecord::Base
   attr_accessor :attendee, :conference
-  after_create :push_to_feeds
+  after_create :push_to_feeds, :save_details
   default_scope lambda { order('created_at desc').limit(50) }
+  
+  def attendee
+    @attendee ||= Attendee.find_by_id(details["attendee_id"])
+  end
+  
+  def conference
+    # no hash with indifferent access
+    #@conference ||= Conference.find_by_id(details[:conference_id])
+    @conference ||= Conference.find_by_id(details["conference_id"])
+  end
+  
   protected
+  
+  def details
+    @details ||= rdb.hgetall
+  end
+  
+  def save_details
+    rdb.hmset(:attendee_id, attendee.id, :conference_id, conference.id)
+  end
   
   def push_to_feeds
     rdb.redis.zadd("events", created_at.to_i, self.id)
